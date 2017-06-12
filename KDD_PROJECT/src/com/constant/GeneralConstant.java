d14be73b3c18e4ee98bb3f3adc489c44d401379a
truonghaibg <truonghaibg@gmail.com>
2017-06-12 13:16:25 +0700

    update

---
+++ KDD_PROJECT/src/com/constant/GeneralConstant.java
@@ -0,0 +1,31 @@
+package com.constant;
+
+public final class GeneralConstant {
+
+	public class SYMBOL {
+		public static final String DOTS = ".";
+		public static final String SPACE = " ";
+		public static final String UNDERSCORE = "_";
+		public static final String SLASH = "/";
+	}
+
+	public class CLASSIFY {
+		public static final String MALE = "male";
+		public static final String MALE_VALUE = "1";
+		public static final String FEMALE = "female";
+		public static final String FEMALE_VALUE = "-1";
+	}
+
+	public static final String INPUT_KDD_CUP_10_PERCENT = "dataset/kddcup.data_10_percent_corrected";
+	public static final String INPUT_KDD_CUP_10_PERCENT_8_PARAMESTER = "dataset/kddcup.data_10_percent_corrected_8_paramester";
+	public static final String INPUT_KDD_CUP_10_PERCENT_RULE = "dataset/kddcup.data_10_percent_corrected_rule";
+
+	public static final String ENCODING_UTF8 = "UTF-8";
+
+	public static String UNI_GRAM = "unigram";
+	public static String BI_GRAM = "bigram";
+	public static String TRI_GRAM = "trigram";
+
+	public static int NUMBER_OF_RANGES = 5;
+
+}
